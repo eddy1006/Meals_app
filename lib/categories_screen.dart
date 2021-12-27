d87437eed0fa7358b9466d1d9b04acccb1b2d9b2
Eddy <adigupta239@gmail.com>
2021-12-28 02:09:57 +0530

    initial commit

---
+++ lib/categories_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'category_item.dart';
+import './dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+        children: DUMMY_CATEGORIES
+            .map((e) => CategoryItem(e.title, e.color))
+            .toList(),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20));
+  }
+}
